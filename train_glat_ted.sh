ca22bf1b1ee5a845aab235765cc1f678c828a7be
宋珍巧 <songzhenqiao@bytedance.com>
2021-11-15 11:22:30 +0800

    update code

--- train_glat_ted.sh
+++ train_glat_ted.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-src=$1
-tgt=$2
-
 export http_proxy=http://lab_mt_caojun.sh:N92TPbyQoQfYkROb@10.124.155.170:8080
 export https_proxy=http://lab_mt_caojun.sh:N92TPbyQoQfYkROb@10.124.155.170:8080
 
@@ -10,51 +7,56 @@ python3 setup.py build_ext --inplace
 pip install .
 pip install sacremoses
 
+src=$1
+tgt=$2
+
 # Download and prepare the data
-data_path=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/songzhenqiao/multilingual_glat/total_data/wmt_en_de_fr_ro_ru_zh
-local_dataset_path=${data_path}
+data_path=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/songzhenqiao/multilingual_glat/total_data/ted_three
+local_dataset_path=${data_path}/KD
 
 local_root=.
 output_path=${local_root}/output
 mkdir -p ${output_path}
 local_checkpoint_path=${output_path}/save_model
-hdfs_checkpoint_path=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/songzhenqiao/multilingual_glat/models/better_ten/transformer_big
+hdfs_checkpoint_path=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/songzhenqiao/multilingual_glat/models/ted_three/GLAT
 remote_checkpoint_path=${hdfs_checkpoint_path}/${src}_${tgt}
 mkdir -p ${local_checkpoint_path}
 hadoop fs -mkdir -p ${hdfs_checkpoint_path}
 hadoop fs -mkdir -p ${remote_checkpoint_path}
-hadoop fs -get ${remote_checkpoint_path}/checkpoint_last.pt ${local_checkpoint_path}
 
 export NCCL_IB_DISABLE=0
 export NCCL_IB_HCA=$ARNOLD_RDMA_DEVICE:1
 export NCCL_IB_GID_INDEX=3
 export NCCL_SOCKET_IFNAME=eth0
 python3 -m torch.distributed.launch --nproc_per_node=$ARNOLD_WORKER_GPU --nnodes=$ARNOLD_NUM  --node_rank=$ARNOLD_ID \
---master_addr=$ARNOLD_WORKER_0_HOST --master_port=$ARNOLD_WORKER_0_PORT fairseq_cli/train_multi_trans.py ${local_dataset_path} \
+--master_addr=$ARNOLD_WORKER_0_HOST --master_port=$ARNOLD_WORKER_0_PORT fairseq_cli/train_mglat.py ${local_dataset_path} \
 --save-dir ${local_checkpoint_path} \
 --remote-save-dir ${remote_checkpoint_path} \
---restore-file ${local_checkpoint_path}/checkpoint_last.pt \
---task multilingual_translation_song \
+--task multilingual_glat_translation \
 --lgs ${src}-${tgt} \
 --mt-steps ${src}-${tgt} \
 --metric-pair ${src}-${tgt} \
+--total-sample-updates 300000 \
+--minus-p 0.3 \
 --dataset-impl "raw" \
---criterion multilingual_label_smoothed_cross_entropy --label-smoothing 0.1 \
---arch multilingual_transformer_song_big \
+--criterion glat_loss --label-smoothing 0.1 \
+--arch MGLAT_base \
+--noise full_mask \
 --optimizer adam \
---adam-betas '(0.9, 0.998)' --adam-eps 1e-6 \
---clip-norm 0.1 \
---lr 3e-4 \
+--adam-betas '(0.9, 0.999)' --adam-eps 1e-6 \
+--clip-norm 2 \
+--lr 5e-4 \
 --lr-scheduler inverse_sqrt \
 --stop-min-lr 1e-9 \
---warmup-updates 10000 \
+--warmup-updates 4000 \
 --warmup-init-lr 1e-7 \
 --dropout 0.3 \
+--annealing-total-num 300000 \
 --weight-decay 0.01 \
---max-tokens 2048 \
---update-freq 4 \
---max-update 400000 \
---max-epoch 300 \
+--max-tokens 8192 \
+--update-freq 1 \
+--max-update 100000 \
+--max-epoch 5000 \
 --fp16 \
 --valid-subset valid \
 --max-sentences-valid 8 \
@@ -66,6 +68,7 @@ python3 -m torch.distributed.launch --nproc_per_node=$ARNOLD_WORKER_GPU --nnodes
 --keep-interval-updates	10 \
 --log-interval 10 \
 --eval-bleu \
+--eval-bleu-args '{"iter_decode_max_iter": 0, "iter_decode_with_beam": 5}' \
 --eval-bleu-detok space \
 --eval-bleu-remove-bpe \
 --eval-bleu-print-samples \
